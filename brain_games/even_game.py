ff94bf7f9980c559fb4bf182800baa082f604db5
Andrey Sukhov <rety2244live@gmail.com>
2024-01-25 15:11:26 +0500

    add even game

--- brain_games/even_game.py
+++ brain_games/even_game.py
@@ -1,9 +1,11 @@
 from random import randint
 import prompt
-from brain_games.scripts.brain_even import user_name
 
 
 def the_even():
+    name = prompt.string("Welcome to the Brain Games!\nMay I have your name?")
+    print(f"Hello {name}!")
+    print('''Answer "yes" if the number is even, otherwise answer "no".''')
     count = 0
     max_score = 3
     while count < max_score:
@@ -15,14 +17,12 @@ def the_even():
             count = count + 1
         elif random_number % 2 != 0 and user_answer.lower() != 'no':
             return (print(user_answer + ''' is wrong answer ;(. Correct answer was 'no'.
-Let's try again, ''' + user_name + '!'))
+Let's try again, ''' + name + '!'))
         elif random_number % 2 != 0 and user_answer.lower() == 'no':
             print('Correct!')
             count = count + 1
         elif random_number % 2 == 0 and user_answer.lower() != 'yes':
             return (print(user_answer + ''' is wrong answer ;(. Correct answer was 'yes'.
-Let's try again, ''' + user_name + '!'))
-    return print('Congratulations, ' + user_name + '!')
+Let's try again, ''' + name + '!'))
+    return print('Congratulations, ' + name + '!')
 
-
-the_even()
--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,17 +1,7 @@
-import prompt
 from brain_games import even_game
-global user_name
-
-
-def welcome_user():
-    user_name = prompt.string('May I have your name? ')
-    print('Hello, ' + user_name + '!')
 
 
 def main():
-    print('Welcome to the Brain Games!')
-    welcome_user()
-    print('Answer "yes" if the number is even, otherwise answer "no".')
     even_game.the_even()
 
 
