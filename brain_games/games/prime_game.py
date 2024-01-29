a93e7762b6d307a1beba63dbc09a51c1e8831046
Andrey Sukhov <rety2244live@gmail.com>
2024-01-29 17:42:41 +0500

    add prime_game

---
+++ brain_games/games/prime_game.py
@@ -0,0 +1,20 @@
+import random
+from brain_games.const import PRIME_INSTRUCTION
+from brain_games.engine import run_game
+
+
+def is_prime(number):
+    for i in range(2, (number//2)+1):
+        if number % i == 0:
+            return False
+    return True
+
+
+def prime_game():
+    number = random.randint(1, 100)
+    result = 'yes' if is_prime(number) else 'no'
+    return number, result
+
+
+def run_prime_game():
+    run_game(prime_game, PRIME_INSTRUCTION)
---
+++ brain_games/scripts/brain_prime.py
@@ -0,0 +1,9 @@
+from brain_games.games.prime_game import run_prime_game
+
+
+def main():
+    run_prime_game()
+
+
+if __name__ == '__main__':
+    main()
