6000663616a3b4dc2eba2da91f1eeae55a967c37
Andrey Sukhov <rety2244live@gmail.com>
2024-01-29 19:05:15 +0500

    fix mistakes

--- brain_games/const.py
+++ brain_games/const.py
@@ -2,7 +2,7 @@ EVEN_INSTRUCTION = f'Answer "yes" if the number is even, otherwise answer "no".'
 CALC_INSTRUCTION = 'What is the result of the expression?'
 GSD_INSTRUCTION = 'Find the greatest common divisor of given numbers.'
 PROGRESS_INSTRUCTION = 'What number is missing in the progression?'
-PRIME_INSTRUCTION = ('Answer "yes" if given number is prime.'
+PRIME_INSTRUCTION = ('Answer "yes" if given number is prime. '
                      'Otherwise answer "no".')
 MATH_SIGNS = ('+', '-', '*')
 MAX_PROGRESSION = 10
